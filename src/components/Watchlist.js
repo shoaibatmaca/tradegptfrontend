2554678d7d3e7ee36f80d2743c135058b094f6ba
Muhammad Shoaib <mshoaibkaglur@gmail.com>
2025-05-27 10:05:57 +0500

    a

--- src/components/Watchlist.js
+++ src/components/Watchlist.js
@@ -449,7 +449,7 @@ const StockWatchlist = ({ sendMessageToChat }) => {
                         rel="noopener noreferrer"
                         className="text-blue-400 flex items-center justify-center gap-1"
                       >
-                        View Historical Data
+                        View Historical Data 
                         <svg viewBox="0 0 24 24" width="12" height="12">
                           <path fill="currentColor" d="M19 19H5V5h7V3H5c-1.11 0-2 .9-2 2v14c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2v-7h-2v7zM14 3v2h3.59l-9.83 9.83 1.41 1.41L19 6.41V10h2V3h-7z"></path>
                         </svg>
